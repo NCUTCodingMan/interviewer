1dc24774e75a99231ac7a581278b0fbb2709021e
NCUTCODINGMAN <1667254420@qq.com>
2017-12-05 11:44:01 +0800

    任意进制的加减法

--- src/main/java/com/rain/operator/Operator.java
+++ src/main/java/com/rain/operator/Operator.java
@@ -1,58 +1,60 @@
 package com.rain.operator;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class Operator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();       
-        int[] array = new int[n];        
-        for (int j = 0;j < n;j ++) {
-            array[j] = scanner.nextInt();
-        }        
-        Random seed = new Random();
-        for (int k = 0;k < array.length;k ++) {
-            swap(array, k, seed.nextInt(n));
-        }        
-        for (int l = 0;l < array.length;l ++) {
-            System.out.print(array[l] + "\t");
-        }       
-        scanner.close();
+        int s = scanner.nextInt();
+        int t = scanner.nextInt();
 
-        int i = 123;
-        System.out.println(i ^ -10);
-
+        parse(n, s, t);
     }
 
-    public static void parse(int n, int s, int t) {
-        char[] s1 = new StringBuilder(s).reverse().toString().toCharArray();
-        char[] s2 = new StringBuilder(t).reverse().toString().toCharArray();
+    private static void parse(int n, int s, int t) {
+        char[] s1 = new StringBuilder(String.valueOf(Math.abs(s))).reverse().toString().toCharArray();
+        char[] s2 = new StringBuilder(String.valueOf(Math.abs(t))).reverse().toString().toCharArray();
 
+        int param1 = transform(s1, n);
+        int param2 = transform(s2, n);
+
+        int result;
+        if (s < 0 && t > 0) {
+            result = - (param1 + param2);
+        } else if (s < 0 && t < 0) {
+            result = param2 - param1;
+        } else if (s > 0 && t > 0) {
+            result = param1 - param2;
+        } else if (s > 0 && t < 0) {
+            result = param1 + param2;
+        } else {
+            result = 0;
+        }
+
+        System.out.println(result);
     }
 
-    public static int power(int n, int count) {
+    private static int power(int n, int count) {
         int sum = 1;
+        if (count == 0) {
+            return 1;
+        }
         for (int i = 0;i < count;i ++) {
             sum *= n;
         }
         return sum;
     }
 
-    public static void transform(char[] s, int n) {
+    private static int transform(char[] s, int n) {
         int digit = 0;
-        for (char c : s) {
+        for (int i = 0;i < s.length;i ++) {
+            if (s[i] - 48 == 0) {
 
+            } else {
+                digit += power(n, i);
+            }
         }
-    }
-
-    public static void transformCharToInt(char c) {
-
-    }
-    
-    public static void swap(int[] array, int source, int des) {
-        int temp = array[source];
-        array[source] = array[des];
-        array[des] = temp;
+        return digit;
     }
 }
