4865f4169781ee0f5744b191f0c6a36be7a672f5
NCUTCodingMan <1667254420@qq.com>
2017-12-05 10:59:32 +0800

    任意进制数的减法

--- src/main/java/com/rain/operator/Operator.java
+++ src/main/java/com/rain/operator/Operator.java
@@ -24,6 +24,31 @@ public class Operator {
         System.out.println(i ^ -10);
 
     }
+
+    public static void parse(int n, int s, int t) {
+        char[] s1 = new StringBuilder(s).reverse().toString().toCharArray();
+        char[] s2 = new StringBuilder(t).reverse().toString().toCharArray();
+
+    }
+
+    public static int power(int n, int count) {
+        int sum = 1;
+        for (int i = 0;i < count;i ++) {
+            sum *= n;
+        }
+        return sum;
+    }
+
+    public static void transform(char[] s, int n) {
+        int digit = 0;
+        for (char c : s) {
+
+        }
+    }
+
+    public static void transformCharToInt(char c) {
+
+    }
     
     public static void swap(int[] array, int source, int des) {
         int temp = array[source];
