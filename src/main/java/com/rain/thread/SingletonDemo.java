1899925878d4eecd0173b752bcf9993ddb7eb651
NCUTCodingMan <1667254420@qq.com>
2017-12-12 17:54:21 +0800

    对volatile的理解

--- src/main/java/com/rain/thread/SingletonDemo.java
+++ src/main/java/com/rain/thread/SingletonDemo.java
@@ -3,22 +3,22 @@ package com.rain.thread;
 public class SingletonDemo {
     private static class RecordExample {
         private int i;
+        // 当flag未声明为volatile时,程序的输出有可能为"....",出现此的原因在于flag的改变对其他线程不可见
         private boolean flag = false;
 
         private void write() {
             i = 1;
-            try {
-                Thread.sleep(1000);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
             flag = true;
+            // 模拟在进行计算,使线程工作内存中flag的值不能立即刷新到主内存中
+            int j = 0;
+            while (j ++ < 100000000);
         }
 
         private void read() {
+            // 根据flag的值判断之前是否已经执行过write()
             if (flag) {
                 int temp = i * i;
-                System.out.println("temp" + temp);
+                System.out.println("temp:" + temp);
             } else {
                 System.out.println("....");
             }
@@ -28,21 +28,23 @@ public class SingletonDemo {
     public static void main(String[] args) {
         final RecordExample example = new RecordExample();
 
-        Thread c = new Thread() {
-            @Override
-            public void run() {
-                example.write();
-            }
-        };
+        while (true) {
+            Thread c = new Thread() {
+                @Override
+                public void run() {
+                    example.write();
+                }
+            };
 
-        Thread d = new Thread() {
-            @Override
-            public void run() {
-                example.read();
-            }
-        };
+            Thread d = new Thread() {
+                @Override
+                public void run() {
+                    example.read();
+                }
+            };
 
-        c.start();
-        d.start();
+            c.start();
+            d.start();
+        }
     }
 }
