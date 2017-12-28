12125e952e38ec6315573e0ed11b5508dfd12183
NCUTCodingMan <1667254420@qq.com>
2017-12-28 22:48:01 +0800

    深入理解ThreadLocal

--- src/main/java/com/rain/date/SimpleDateFormatDemo.java
+++ src/main/java/com/rain/date/SimpleDateFormatDemo.java
@@ -1,8 +1,10 @@
 package com.rain.date;
 
+import java.lang.reflect.Field;
 import java.text.SimpleDateFormat;
 
 /**
+ * @author rain
  * created on 2017/12/28
  * SimpleDateFormat
  *  SimpleDateFormat.parse()不是线程安全的, 根本原因在于SimpleDateFormat继承DateFormat
@@ -17,20 +19,41 @@ import java.text.SimpleDateFormat;
  * 链接 http://ifeve.com/notsafesimpledateformat/
  */
 public class SimpleDateFormatDemo {
-    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private static ThreadLocal<SimpleDateFormat> threadLocal = new ThreadLocal<SimpleDateFormat>() {
+        @Override
+        protected SimpleDateFormat initialValue() {
+            return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        }
+    };
+
+    private static final int COUNT = 10;
 
     public static void main(String[] args) {
         int i = 0;
-        while (i < 10) {
-            Thread thread = new Thread(new Runnable() {
+        while (i < COUNT) {
+            final Thread thread = new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    synchronized (simpleDateFormat) {
-                        try {
-                            System.out.println(simpleDateFormat.parse("2017-12-28 11:00:00"));
-                        } catch (Exception e) {
-                            e.printStackTrace();
+                    try {
+                        System.out.println(threadLocal.get().parse("2017-12-28 11:00:00"));
+
+                        Thread thread = Thread.currentThread();
+                        Field[] fields = thread.getClass().getDeclaredFields();
+                        for (Field field : fields) {
+                            field.setAccessible(true);
+                            try {
+                                System.out.println(field.getName() + "\t" + field.get(thread));
+                                String name = "threadLocals";
+                                if (field.getName().equals(name)) {
+                                    Object object = field.get(thread);
+                                    System.out.println(object);
+                                }
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
                         }
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
                 }
             });
@@ -39,5 +62,8 @@ public class SimpleDateFormatDemo {
 
             i ++;
         }
+
+
     }
+
 }
---
+++ src/main/java/com/rain/thread/ThreadLocalDemo.java
@@ -0,0 +1,7 @@
+package com.rain.thread;
+
+/**
+ * created on 2017/12/28
+ */
+public class ThreadLocalDemo {
+}
